4720bd67938ee32428add7931837c449111e411f
hwahlberg <wahlberg.hakan@gmail.com>
2016-01-20 15:03:24 +0100

    added sync-command

--- backup2usb.sh
+++ backup2usb.sh
@@ -3,14 +3,22 @@
 cd $HOME
 
 for dir in \
+    AndroidStudioProjects \
     bin \
     Documents \
+    Dropbox \
+    lib \
+    NetBeansProjects \
     Photos \
-    Pictures 
+    PythonProjects \
+    sql
 do
     echo -e "\n############   \"$dir\" ..."
     rsync -avL --delete "$dir"  /usbbackup/home_hwa/Backup
 done
+
+sync
+sync
 
 
 exit 0
